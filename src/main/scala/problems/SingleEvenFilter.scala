dcf3c4e454d4fcc8cc3505256115623a6ef5c9bb
perinze <perinzefaper@foxmail.com>
2023-05-28 03:41:11 +0800

    single even filter

--- src/main/scala/problems/SingleEvenFilter.scala
+++ src/main/scala/problems/SingleEvenFilter.scala
@@ -28,21 +28,28 @@ class PredicateFilter[T <: Data](dtype: T, f: T => Bool) extends Filter(dtype) {
 object SingleFilter {
   def apply[T <: UInt](dtype: T) = 
     // Change function argument of Predicate filter below ----------
-    Module(new PredicateFilter(dtype, (x: T) => false.B))
+    Module(new PredicateFilter(dtype, (x: T) => x <= 9.U))
     // Change function argument of Predicate filter above ----------
 }
 
 object EvenFilter {
   def apply[T <: UInt](dtype: T) = 
     // Change function argument of Predicate filter below ----------
-    Module(new PredicateFilter(dtype, (x: T) => false.B))
+    Module(new PredicateFilter(dtype, (x: T) => x(0) === 0.U))
     // Change function argument of Predicate filter above ----------
 }
 
 class SingleEvenFilter[T <: UInt](dtype: T) extends Filter(dtype) {
   // Implement composition below ----------
 
-  io.out <> io.in
+  val singleFilter = SingleFilter.apply(dtype)
+  val evenFilter = EvenFilter.apply(dtype)
+  singleFilter.io.in.valid := io.in.valid
+  evenFilter.io.in.valid := io.in.valid
+  singleFilter.io.in.bits := io.in.bits
+  evenFilter.io.in.bits := io.in.bits
+  io.out.valid := singleFilter.io.out.valid & evenFilter.io.out.valid
+  io.out.bits := io.in.bits
 
   // Implement composition above ----------
 }
